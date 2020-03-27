d0e127746f088a20a027aaedf74cfb406901f086
Don-Stevenson <dsteven1@gmail.com>
2020-03-27 00:03:32 -0400

    handle search input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Search from './components/Search'
-
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 function App() {
+  const [state, setState] = useState({ s: "", results: [], selected: {} });
+  const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=3d903a27";
+  const handleInput = e => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s };
+    });
+    console.log(state.s);
+  };
+
   return (
     <div className="App">
       <header className="App">
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function Search() {
-  return <section className="search">
-      <input type="text" placeholder="Search for a movie..." className="searchbox"/>
-  </section>;
+function Search({ handleInput }) {
+  return (
+    <section className="search">
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        className="searchbox"
+        onChange={handleInput}
+      />
+    </section>
+  );
 }
 
 export default Search;
