c4395424553863a27bd3ff558cbf21ff090c3a00
Don-Stevenson <dsteven1@gmail.com>
2020-03-27 02:35:26 -0400

    handle a blank search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
-import Search from './components/Search'
-import Results from './components/Results'
-import Popup from './components/Popup'
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Popup from "./components/Popup";
 
 function App() {
   const [state, setState] = useState({
@@ -13,25 +13,30 @@ function App() {
   });
   const apiurl = "http://www.omdbapi.com/?apikey=3d903a27";
 
-  const search = (e) => {
+  const search = e => {
     if (e.key === "Enter") {
       axios(apiurl + "&s=" + state.s).then(({ data }) => {
-        let results = data.Search;
+        if (data.search === undefined) {
+          alert("Error! please enter a valid title");
+        } else {
+          console.log("data is", data.search);
+          let results = data.Search;
 
-        setState(prevState => {
-          return { ...prevState, results: results }
-        })
+          setState(prevState => {
+            return { ...prevState, results: results };
+          });
+        }
       });
     }
-  }
-  
-  const handleInput = (e) => {
+  };
+
+  const handleInput = e => {
     let s = e.target.value;
 
     setState(prevState => {
-      return { ...prevState, s: s }
+      return { ...prevState, s: s };
     });
-  }
+  };
 
   const openPopup = id => {
     axios(apiurl + "&i=" + id).then(({ data }) => {
@@ -40,16 +45,16 @@ function App() {
       console.log(result);
 
       setState(prevState => {
-        return { ...prevState, selected: result }
+        return { ...prevState, selected: result };
       });
     });
-  }
+  };
 
   const closePopup = () => {
     setState(prevState => {
-      return { ...prevState, selected: {} }
+      return { ...prevState, selected: {} };
     });
-  }
+  };
 
   return (
     <div className="App">
@@ -61,11 +66,14 @@ function App() {
 
         <Results results={state.results} openPopup={openPopup} />
 
-        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+        {typeof state.selected.Title != "undefined" ? (
+          <Popup selected={state.selected} closePopup={closePopup} />
+        ) : (
+          false
+        )}
       </main>
     </div>
   );
 }
 
-export default App
-
+export default App;
