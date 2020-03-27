167659f4b5075e453cd9af9cbfb3a3173873ef91
Don-Stevenson <dsteven1@gmail.com>
2020-03-26 23:42:11 -0400

    add search box

---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Search() {
+  return <section className="search">
+      <input type="text" placeholder="Search for a movie..." className="searchbox"/>
+  </section>;
+}
+
+export default Search;
